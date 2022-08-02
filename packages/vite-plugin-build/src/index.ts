a03d021f9cf6779fa1b753c8251268b4fc3dd57e
samonxian <samonxian@tencent.com>
2022-08-02 23:47:24 +0800

    refactor: 💡 使用 config 构建函数重构一些逻辑

--- packages/vite-plugin-build/src/index.ts
+++ packages/vite-plugin-build/src/index.ts
@@ -1,9 +1,8 @@
 import path from 'path';
 import fs from 'fs-extra';
-import { normalizePath } from 'vite';
-import type { Plugin, ResolvedConfig } from 'vite';
+import type { Plugin, UserConfig } from 'vite';
 import type { BuildFilesOptions } from './buildFiles';
-import { buildFiles, transformFile } from './buildFiles';
+import { buildFiles } from './buildFiles';
 import { InterceptConsole } from './InterceptConsole';
 import type { BuildLibOptions } from './buildLib';
 import { buildLib } from './buildLib';
@@ -42,162 +41,121 @@ const pluginName = 'vite:build';
 const defaultInputFolder = 'src';
 const defaultEsOutputDir = 'es';
 const defaultCommonJsOutputDir = 'lib';
-let shouldRun = true;
 let reporter: ReturnType<typeof createReporter>;
 export const interceptConsoleInstance = new InterceptConsole();
 
 export function buildPlugin(options: Options = {}): Plugin {
   const { fileBuild = {}, libBuild } = options;
-  let config: ResolvedConfig;
+  let userConfig: UserConfig;
+  let isSvelte = false;
+  let isVue = false;
 
   return {
     name: pluginName,
     apply: 'build',
 
-    configResolved(resolvedConfig) {
-      if (!shouldRun) {
-        return;
-      }
-
-      // 存储最终解析的配置
-      config = resolvedConfig;
-      reporter = createReporter(resolvedConfig);
-      // 修改当前的构建
+    config(config) {
+      // 修改当前的构建，当前构建在 build 插件中只有触发构建作用
       const tempTsFilePath = path.resolve(process.cwd(), 'node_modules/.temp/noop.ts');
       fs.ensureFileSync(tempTsFilePath);
       fs.writeFileSync(tempTsFilePath, 'const a = 2', { encoding: 'utf-8' });
-      config.build.rollupOptions = {
-        output: { dir: path.resolve(process.cwd(), 'node_modules/.temp/dist') },
+      config.build = {
+        emptyOutDir: false,
+        rollupOptions: {
+          output: { dir: path.resolve(process.cwd(), 'node_modules/.temp/dist') },
+        },
+        lib: {
+          entry: tempTsFilePath,
+          name: 'noop',
+          formats: [],
+        },
       };
-      config.build.emptyOutDir = false;
-      config.build.lib = {
-        entry: tempTsFilePath,
-        name: 'noop',
-        formats: [],
-      };
+
+      // 过滤 vite-plugin-build 插件
+      config.plugins = config.plugins.filter((p) => {
+        const lp = (p || {}) as Record<string, any>;
+
+        if (lp.name === pluginName) {
+          return false;
+        }
+
+        return true;
+      });
+
+      // 判断是否是 svelte 或者 vue app
+      config.plugins.flat(10).forEach((p) => {
+        const lp = (p || {}) as Record<string, any>;
+
+        if (lp.name === 'vite-plugin-svelte') {
+          isSvelte = true;
+        }
+
+        if (lp.name === 'vite:vue') {
+          isVue = true;
+        }
+      });
+
+      userConfig = config;
     },
 
-    async buildStart() {
-      if (!shouldRun) {
-        return;
-      }
+    configResolved(resolvedConfig) {
+      reporter = createReporter(resolvedConfig);
+    },
+
+    buildStart() {
       interceptConsoleInstance.silent(); // 拦截 console.log 和 console.warn 不做任何输出
     },
 
     async closeBundle() {
-      if (!shouldRun) {
-        return;
+      const shouldBuildFiles = typeof fileBuild === 'object';
+      const lastFileBuildOptions = typeof fileBuild === 'object' ? fileBuild : {};
+      const lastEsOutputDir = lastFileBuildOptions.esOutputDir ?? defaultEsOutputDir;
+      const lastCommonjsOutputDir = lastFileBuildOptions.commonJsOutputDir ?? defaultCommonJsOutputDir;
+
+      if (shouldBuildFiles && fileBuild.emitDeclaration) {
+        // 生成声明文件
+        const { inputFolder = defaultInputFolder } = fileBuild;
+
+        emitDeclaration({
+          rootDir: inputFolder,
+          commonJsOutputDir: lastCommonjsOutputDir,
+          esOutputDir: lastEsOutputDir,
+          isSvelte: fileBuild.isSvelte || isSvelte,
+          isVue: fileBuild.isVue || isVue,
+        });
       }
 
-      shouldRun = false;
-      const tempViteConfigFilePath = path.resolve(process.cwd(), 'node_modules/.temp/.temp.vite.config.js');
+      const pluginHooks: Plugin = {
+        name: 'noop // 无用，为了防止 eslint 报错',
+        transform: (_, id) => {
+          reporter.transform(id);
+          return null;
+        },
 
-      try {
-        // 由于 rollup 的动态 import 不支持单变量引入 ts 和 js 文件，同时也存在循环引用问题
-        // 所以直接转换 vite 配置文件为临时的 commonjs 格式，在通过 require 引入。
-        await transformFile(path.basename(config.configFile), {
-          viteConfig: {
-            logLevel: 'silent',
-          },
-          esOutputDir: false,
-          rollupOptionsOutput: { file: tempViteConfigFilePath },
-          rollupOptionsExternal(id) {
-            const normalizeId = normalizePath(id);
-            if (
-              /^\//.test(id) ||
-              /^\\/.test(id) ||
-              /^\.\//.test(id) ||
-              /^\.\.\//.test(id) ||
-              normalizeId === config.configFile
-            ) {
-              // 本地文件则一起打包到一个文件中
-              return false;
-            }
-            return true;
-          },
-        });
-
-        // 获取 vite 配置
-        let viteConfig = require(tempViteConfigFilePath).default;
-        if (typeof viteConfig === 'function') {
-          viteConfig = viteConfig({ mode: config.mode, command: config.command });
-        }
-
-        let isSvelte = false;
-        let isVue = false;
-
-        // 过滤 vite-plugin-build 插件
-        viteConfig.plugins = viteConfig.plugins.filter((p: Record<string, any>) => {
-          if (p.name === pluginName) {
-            return false;
-          }
-
-          if (p.name === 'vite:vue') {
-            isVue = true;
-          }
-
-          if (Array.isArray(p)) {
-            p.forEach((v) => {
-              if (v.name === 'vite-plugin-svelte') {
-                isSvelte = true;
-              }
-            });
-          }
-
-          return true;
-        });
-
-        const shouldBuildFiles = typeof fileBuild === 'object';
-        const lastFileBuildOptions = typeof fileBuild === 'object' ? fileBuild : {};
-        const lastEsOutputDir = lastFileBuildOptions.esOutputDir ?? defaultEsOutputDir;
-        const lastCommonjsOutputDir = lastFileBuildOptions.commonJsOutputDir ?? defaultCommonJsOutputDir;
-
-        if (shouldBuildFiles && fileBuild.emitDeclaration) {
-          // 生成声明文件
-          const { inputFolder = defaultInputFolder } = fileBuild;
-
-          emitDeclaration({
-            rootDir: inputFolder,
-            commonJsOutputDir: lastCommonjsOutputDir,
+        writeBundle: (_, output) => {
+          reporter.writeBundle(output);
+        },
+      };
+      const buildPromise = [
+        shouldBuildFiles &&
+          buildFiles({
+            ...lastFileBuildOptions,
             esOutputDir: lastEsOutputDir,
-            isSvelte: fileBuild.isSvelte || isSvelte,
-            isVue: fileBuild.isVue || isVue,
-          });
-        }
+            commonJsOutputDir: lastCommonjsOutputDir,
+            viteConfig: userConfig,
+            pluginHooks,
+          }),
+        libBuild &&
+          buildLib({
+            ...libBuild,
+            viteConfig: userConfig,
+            pluginHooks,
+          }),
+      ].filter(Boolean);
+      await Promise.all(buildPromise);
 
-        const pluginHooks: Plugin = {
-          name: 'noop // 无用，为了防止 eslint 报错',
-          transform: (_, id) => {
-            reporter.transform(id);
-            return null;
-          },
+      reporter.buildEndAll();
 
-          writeBundle: (_, output) => {
-            reporter.writeBundle(output);
-          },
-        };
-        const buildPromise = [
-          shouldBuildFiles &&
-            buildFiles({
-              ...lastFileBuildOptions,
-              esOutputDir: lastEsOutputDir,
-              commonJsOutputDir: lastCommonjsOutputDir,
-              viteConfig,
-              pluginHooks,
-            }),
-          libBuild &&
-            buildLib({
-              ...libBuild,
-              viteConfig,
-              pluginHooks,
-            }),
-        ].filter(Boolean);
-        await Promise.all(buildPromise);
-        reporter.buildEndAll();
-      } finally {
-        // 删除临时的 vite 配置文件
-        fs.removeSync(tempViteConfigFilePath);
-      }
       interceptConsoleInstance.restore(); // 恢复 console.log 和 console.warn
     },
   };
